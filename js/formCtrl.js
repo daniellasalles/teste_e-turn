cd0ba91fa79b853570a5264443d3e3c8faa4f9eb
Daniella Salles <daniellac.salles@gmail.com>
2020-03-29 12:29:26 -0300

    Organizando o código

--- js/formCtrl.js
+++ js/formCtrl.js
@@ -1,47 +1,33 @@
 angular.module("form").controller("formCtrl", function($http, $scope){
-    //$scope.app = "form";
-    
+    // Cria array para armazenar dados ddos contatos cadastrados
     $scope.contatos = [{ }];
-        /*nome: '',
-        telefone: '',
-        email: '',
-        observacao: '',
-        endereco: {
-            cep: '',
-            logradouro: '',
-            numero: '',
-            complemento: '',
-            bairro: '',
-            cidade: '',
-            uf: ''
-        }*/
     
-    $scope.adicionaContato = function(contato) {
+
+    // Adiciona os dados do novo contato no array de contatos
+    $scope.adicionaContato = function(contato, contatos) {
         $scope.contatos.push(angular.copy(contato));
-        //saveToStorage(contato);
         delete $scope.contato;
     };
     
-
+    // Faz a busca do CEP usando a API Postmon
     $scope.buscaCep = function() {
         $http({
             method: 'GET',
-            url:'https://api.postmon.com.br/v1/cep/' + $scope.contato.endereco.cep ,
-            
+            url:'https://api.postmon.com.br/v1/cep/' + $scope.contato.endereco.cep,
+
+        // Caso a conexão seja bem sucedida, preenche automaticamente os campos de endereço com o resultado encontrado
         }).then(function(response){
             $scope.errorMessage = null;
             $scope.contato.endereco.logradouro = response.data.logradouro;
             $scope.contato.endereco.bairro = response.data.bairro;
             $scope.contato.endereco.cidade = response.data.cidade;
             $scope.contato.endereco.uf = response.data.estado;
+
+        // Caso aconteça erro na conexão, retorna alerta de erro
         }).catch(function(reject){
-            $scope.errorMessage = "Cep não encontrado";
+            $scope.errorCep = "Cep não encontrado";
         });
     }
-
-        /*$scope.saveToStorage = function($scope) {
-            localStorage.setItem("lista_contato", JSON.stringify($scope.contato));
-        }*/
 
 });
 
